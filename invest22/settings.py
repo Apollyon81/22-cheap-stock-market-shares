fe68036065793e1b0c31643333ffcc4eb6ec47bc
Apollyon81 <taikeramos_1998@hotmail.com>
2023-10-12 11:24:16 -0300

    002

--- invest22/settings.py
+++ invest22/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'structure',
+    "structure",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static') 
     ]
 
 
