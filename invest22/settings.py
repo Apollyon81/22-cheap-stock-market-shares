e34baf53a7b7d1f65e787ff0b4d4cc71447fcd75
Apollyon81 <taikeramos_1998@hotmail.com>
2023-10-13 15:23:39 -0300

    003

--- invest22/settings.py
+++ invest22/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "us-en"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "US/san_francisco"
 
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-    ]
+]
 
 
 
