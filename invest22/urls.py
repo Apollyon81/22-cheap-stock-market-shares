c61c1544f4bf59d3000c785619fb43e6198001f8
Taique.mr <taikeramos_1998@hotmail.com>
2025-08-06 23:16:33 -0300

    Update HTML with stock selection filters

--- invest22/urls.py
+++ invest22/urls.py
@@ -2,7 +2,7 @@
 URL configuration for invest22 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('home/', include('structure.urls'))
+    path('admin/', admin.site.urls),
+]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('structure.urls')),  # Rota raiz vai para o app structure
 ]
--- structure/views.py
+++ structure/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
-
+    return render(request, 'structure/index.html')
